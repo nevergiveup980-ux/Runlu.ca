0dfb47ea957517e41ef1f1903966a92a54b276a1
nevergiveup980-ux <nevergiveup980@gmail.com>
2026-09-20 09:56:55 -0600

    Add disabled commerce configuration

---
+++ commerce-config.js
@@ -0,0 +1,12 @@
+window.RUNLUCommerceConfig = Object.freeze({
+  provider: "lemonsqueezy",
+  products: Object.freeze({
+    "sbcc-v1-1": Object.freeze({
+      enabled: false,
+      checkoutUrl: "",
+      price: "CAD $19",
+      version: "1.1",
+      delivery: "provider-hosted-download"
+    })
+  })
+});
