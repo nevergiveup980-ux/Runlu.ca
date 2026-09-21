6216cc13ac081fc1103ca064d81628eef6f84562
nevergiveup980-ux <nevergiveup980@gmail.com>
2026-09-20 21:28:52 -0600

    Add lightweight RUNLU Bag
    * Add product metadata for RUNLU Bag
    
    * Add lightweight persistent RUNLU Bag drawer
    
    * Load RUNLU Bag commerce UI
    
    * Load RUNLU Bag commerce UI
    
    * Load RUNLU Bag commerce UI

--- commerce-config.js
+++ commerce-config.js
@@ -2,6 +2,7 @@ window.RUNLUCommerceConfig = Object.freeze({
   provider: "stripe",
   products: Object.freeze({
     "sbcc-v1-1": Object.freeze({
+      name: "Small Business Command Center V1.1",
       enabled: true,
       checkoutUrl: "https://buy.stripe.com/9B6cN61eP46r0M4cNw6Vq00",
       price: "CAD $9.99",
