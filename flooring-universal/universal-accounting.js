a1bbed7dc0e31004d6fbe22dd839093d16b9de55
nevergiveup980-ux <nevergiveup980@gmail.com>
2026-09-22 22:47:47 -0600

    Route Universal business storage through data adapter

--- flooring-universal/universal-accounting.js
+++ flooring-universal/universal-accounting.js
@@ -3,7 +3,7 @@
 (function(){
 'use strict';
 const STORE='runlu_flooring_universal_u1_supplier_accounting',WS='runlu_flooring_universal_u0_workspace';
-const read=(k,d)=>{try{return JSON.parse(localStorage.getItem(k)||'null')??d}catch(_){return d}},write=(k,v)=>localStorage.setItem(k,JSON.stringify(v)),esc=v=>String(v??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m])),num=v=>Number.isFinite(Number(v))?Number(v):0;
+const read=(k,d)=>window.RUNLUUniversalData.read(k,d),write=(k,v)=>window.RUNLUUniversalData.write(k,v),esc=v=>String(v??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m])),num=v=>Number.isFinite(Number(v))?Number(v):0;
 const org=()=>read(WS,null)?.company?.organizationId||'',all=()=>read(STORE,[]),rows=()=>all().filter(x=>x.organizationId===org());
 function save(xs){write(STORE,[...all().filter(x=>x.organizationId!==org()),...xs])}
 function orderedTotal(t){return (t.items||[]).reduce((s,x)=>s+num(x.qty)*num(x.unitPrice),0)}
